06a9a6103d86a3d1cba0b85f7824e84df4c3ae83
jeet-webdev <jeetdev1988@gmail.com>
2026-04-13 10:12:47 +0530

    little change in navigation

--- src/app/(private)/dashboard/administration/page.tsx
+++ src/app/(private)/dashboard/administration/page.tsx
@@ -3,4 +3,3 @@ import RoleDashboardPage from "@/features/dashboard/pages/RoleDashboardPage";
 export default function AdministrationDashboardPage() {
   return <RoleDashboardPage expectedRole="ADMINISTRATION" />;
 }
-//
